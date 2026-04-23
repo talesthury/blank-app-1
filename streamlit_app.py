6aa837d05a1e860f57c6b4d3afccbfe8649c5a9a
talesthury <talesthury@gmail.com>
2026-04-23 07:19:00 +0000

    hahaha

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,21 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
 
 
 
