18bcbe493845948fd5c4e3654232da862e86bf4d
talesthury <talesthury@gmail.com>
2026-04-23 09:57:37 +0200

    djenjdnej

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,5 +38,22 @@ with tab3:
 
 
 
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+    st.image("https://static.streamlit.io/examples/dice.jpg")
+    
+
+#      kjxnjxw
+
+
+
+
+
 
 
