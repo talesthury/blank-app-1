463b429e5b399b2f3357fa427d832736e254e89e
talesthury <talesthury@gmail.com>
2026-04-23 07:13:44 +0000

    whatever

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My xzsgxzsx new app")
+st.title("🎈 My new app Tales")
+st.subheader("Hello")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+
+
+
+
+
+
+
+
